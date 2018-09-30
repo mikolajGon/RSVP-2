5499e5b1343506cdfe5002e4467f32a50aa0c54a
mikolajGon <mik.gonciarz@gmail.com>
2018-09-30 11:38:58 +0200

    changed whole logic of JS, minor changes to HTML

--- js/app.js
+++ js/app.js
@@ -1,89 +1,120 @@
-document.addEventListener(`DOMContentLoaded`, () =>{
+const registrar = document.querySelector(`#registrar`);
+const addInviteeInput = registrar.querySelector(`#add_invitee_input`);
+const invitedUl = document.querySelector(`#invitedList`);
+const hideNotRespondedCheckbox = document.querySelector(`.main input`);
+const emptyInviteeTile = document.querySelector('#empty_invitee').firstElementChild;
 
-	const form = document.querySelector(`#registrar`);
-	const formInput = form.querySelector(`input`);
-	const invitedUl = document.querySelector(`#invitedList`);
-	const hideNotResponded = document.querySelector(`.main input`);
+// added one user add beggining for testing purpose
+const Czarek = new Invitee(0, 'Czarek', true);
 
-	function newElement(elementParent, element) {
-		const name = document.createElement(element);
-		elementParent.appendChild(name)
-		return name
-	}
+let invitees = [Czarek];
+let nextId = invitees.length;;
 
-	function text(elementParent, element, text) {
-		const name = newElement(elementParent, element);
-		name.textContent = text;
-		return name;
-	}
+/* -----------------
+ Object Functions
+------------------ */
 
-	function type(elementParent, element, type) {
-		const name = newElement(elementParent, element);
-		name.type = type;
-		return name;
-	}
+function addNewInvitee() {
+	const newInvitee = new Invitee(nextId, addInviteeInput.value);
+	invitees = [...invitees, newInvitee];
+	addInviteeInput.value = ``;
+	nextId++
+}
 
-	function submitForm() {
-		const newLi = newElement(invitedUl, 'li');
-		const newSpan = text(newLi, `span`, formInput.value);
-		const newInput = type(newLi, `input`, 'text');
-		newInput.value = formInput.value;
-		newInput.classList.add(`invisible`);
-		const label = text(newLi, `label`, `Confirmed`);
-		label.setAttribute(`for`, `confirmed`);
-		const checkbox = type(label, 'input', `checkbox`);
-		checkbox.setAttribute(`name`, `confirmed`);
-		const edit = text(newLi, 'button', 'Edit');
-		const remove = text(newLi, 'button', `Remove`);
-	}
+function indexOfId(id) {
+	const index = invitees.findIndex(invitee => invitee.id === id);
+	return index;
+}
 
-
-	form.addEventListener(`submit`, (e) => {
-		e.preventDefault();
-		submitForm();
-		formInput.value = ``;
-	});
-
-	invitedUl.addEventListener('change', (e) => e.target
-			.parentElement
-			.parentElement
-			.classList.toggle(`responded`));
-
-	invitedUl.addEventListener(`click`, (e) => {
-		if (e.target.tagName === 'BUTTON') {
-				function edit(className, text) {
-						name.classList.toggle(className);
-						input.classList.toggle(className);
-						button.textContent = text;
-				};
-				const button = e.target;
-				const li = button.parentNode;
-				const ul = li.parentNode;
-				const name = li.firstElementChild;
-				const input = name.nextElementSibling;
-				const nameActions = {
-					Remove: () => ul.removeChild(li),
-					Edit: () => edit(`invisible`, `Save`),
-					Save: () => {
-							name.textContent = input.value;
-							edit(`invisible`, `Edit`);
-					}
-				};
-				nameActions[button.textContent]();
+function editOrRemoveInvitee(button) {
+	const buttonName = button.textContent.toLowerCase();
+	const inviteeId = parseInt(button.dataset[buttonName]);
+	const inviteeIndex = indexOfId(inviteeId);
+	const invitee = invitedUl.querySelector(`#invitee-${inviteeId}`);
+	const buttonActions = {
+		remove: () => {
+			invitees = [
+				...invitees.slice(0,inviteeIndex),
+				...invitees.slice(inviteeIndex + 1)
+			];
+			renderAllInvitees();
+		},
+		edit: () => {
+			invitee.querySelector('[data-name]').classList.add('invisible');
+			invitee.querySelector('[data-input]').classList.remove('invisible');
+			button.textContent = `Save`;
+		},
+		save: () => {
+			const newName = invitee.querySelector('[data-input]').value;
+			invitees[inviteeIndex].changeName(newName);
+			renderAllInvitees();
 		}
-	});
+	};
+	buttonActions[buttonName]();
+}
 
-	hideNotResponded.addEventListener(`change`, (e) => {
-		const isChecked = e.target.checked;
-		const invitedLi = [...invitedUl.children];
-		invitedLi.forEach(li => {
-			const checkbox = li.querySelector(`label input`);
-			if (isChecked) {
-				(!checkbox.checked) ?	li.classList.add(`invisible`) : li.classList.remove(`invisible`);
-			} else {
-				li.classList.remove(`invisible`);
-			}
-		});
-	});
+/* -----------------
+ Rendering Function
+------------------ */
 
+function renderAllInvitees() {
+	while (invitedUl.firstChild) {
+		invitedUl.removeChild(invitedUl.firstChild);
+	}
+	const isChecked = hideNotRespondedCheckbox.checked;
+	let inviteesToDisplay;
+	if (isChecked) {
+		inviteesToDisplay = invitees.filter(invitee => invitee.confirmed);
+	} else{
+		inviteesToDisplay = invitees;
+	}
+	const docFrag = document.createDocumentFragment();
+	inviteesToDisplay.forEach(invitee => {
+		const inviteeTile = emptyInviteeTile.cloneNode(true);
+		inviteeTile.setAttribute('id', `invitee-${invitee.id}`);
+		inviteeTile.querySelector('[data-name]').textContent = invitee.name;
+		inviteeTile.querySelector('[data-input]').value = invitee.name;
+		inviteeTile.querySelector('[data-input]').dataset.input = invitee.id;
+		inviteeTile.querySelector('[data-edit]').dataset.edit = invitee.id;
+		inviteeTile.querySelector('[data-save]').dataset.save = invitee.id;
+		inviteeTile.querySelector('[data-remove]').dataset.remove = invitee.id;
+		inviteeTile.querySelector('[data-confirmed]').dataset.confirmed = invitee.id;
+		inviteeTile.querySelector('[name="confirmed"]').checked = invitee.confirmed;
+		if (invitee.confirmed) inviteeTile.classList.add('responded');
+		docFrag.appendChild(inviteeTile);
+	});
+	invitedUl.appendChild(docFrag);
+}
+
+/* -----------------
+		Listeners
+------------------ */
+
+document.addEventListener('DOMContentLoaded', renderAllInvitees);
+
+registrar.addEventListener(`submit`, (e) => {
+	e.preventDefault();
+	if (addInviteeInput.value.length < 3) alert('Invitee name must be at least 3 characters');
+	else {
+		addNewInvitee();
+		renderAllInvitees();
+		addInviteeInput.value = '';
+	};
 });
+
+invitedUl.addEventListener('change', (e) => {
+	if (e.target.name === "confirmed") {
+		const inviteeId = parseInt(e.target.dataset.confirmed);
+		const inviteeIndex = indexOfId(inviteeId);
+		invitees[inviteeIndex].changeConfirmedStatus();
+		renderAllInvitees();
+	}
+});
+
+invitedUl.addEventListener(`click`, (e) => {
+	if (e.target.tagName === 'BUTTON') {
+		editOrRemoveInvitee(e.target);
+	}
+});
+
+hideNotRespondedCheckbox.addEventListener(`change`, renderAllInvitees);
